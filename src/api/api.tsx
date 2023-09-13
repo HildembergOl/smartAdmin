60e03131a98cf7d06f67c16451c9c73393036aa0
HildembergOl <89931099+HildembergOl@users.noreply.github.com>
2023-09-13 09:18:12 -0300

    13.09.2023 09:18

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -5,7 +5,7 @@ const token = `Bearer ${localStorage.getItem('TOKEN_API')}`
 
 const api = axios.create()
 
-api.defaults.baseURL = 'http://localhost:4000/'
+api.defaults.baseURL = 'http://localhost:4000/api/'
 api.defaults.timeout = 10000
 api.defaults.headers.common.Authorization = token
 
